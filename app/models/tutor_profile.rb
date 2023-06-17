533b4a55da485e996779a403c2ffa35e32cc792f
Katleho Zonke <zonkekatleho@gmail.com>
2023-06-17 11:18:22 +0200

    Updated validations

--- app/models/tutor_profile.rb
+++ app/models/tutor_profile.rb
@@ -1,4 +1,10 @@
 class TutorProfile < ApplicationRecord
   belongs_to :user
-  belongs_to :student_reviews
+  has_many :student_reviews
+
+  validates :bio, presence: true, length: { maximum: 300 }
+  validates :skills, presence: true
+  validates :location, presence: true
+  validates :email, presence: true
+
 end
